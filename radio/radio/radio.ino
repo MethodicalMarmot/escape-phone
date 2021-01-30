ddb54a5b9a1b123195a81bb3a6168caf1337d125
Semko, Georgii <ext-georgii.semko@here.com>
2021-01-30 11:13:31 +0200

    radio

---
+++ radio/radio/radio.ino
@@ -0,0 +1,95 @@
+#include <SD.h>
+#include <TMRpcm.h>
+
+const int debounceTimeout = 100;
+const int enableActiveState = LOW;
+char* filename = "000.wav";
+
+const byte enablePin = 2;
+const byte tvPin = 3;
+
+const byte phonePin = 9;
+const byte chipSelect = 10;
+TMRpcm tmrpcm;
+
+// set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+
+void setup() {
+  Serial.begin(9600);
+  
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+    Serial.println("initialization failed");
+    return;
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+
+  // print the type of card
+  Serial.print("\nCard type: ");
+  switch (card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    return;
+  }
+
+  if (!SD.begin(chipSelect)) {
+    return;
+  }
+
+  tmrpcm.speakerPin = phonePin;
+  tmrpcm.setVolume(7);
+  tmrpcm.loop(0);
+  tmrpcm.quality(1);
+
+  pinMode(enablePin, INPUT_PULLUP);
+  pinMode(tvPin, OUTPUT);
+  
+  Serial.println("INIT");
+}
+
+int lastValue = enableActiveState == HIGH ? LOW : HIGH;
+unsigned long prevValueMillis = millis();
+void loop() {
+  unsigned long currentMillis = millis();
+
+  int value = currentMillis - prevValueMillis > debounceTimeout ? digitalRead(enablePin) : lastValue;
+  
+  boolean state = value == enableActiveState;
+    
+  if (value != lastValue) {
+    prevValueMillis = currentMillis;
+    lastValue = value;
+
+    if (state) {
+      tmrpcm.play(filename);
+      digitalWrite(tvPin, HIGH);
+      Serial.println("ENABLED");
+    } else {
+      tmrpcm.stopPlayback();
+      digitalWrite(tvPin, LOW);
+      Serial.println("DISABLED");
+    }
+  }
+
+  if (state && !tmrpcm.isPlaying()) {
+    tmrpcm.play(filename);
+    Serial.println("REPEAT");
+  }
+}
