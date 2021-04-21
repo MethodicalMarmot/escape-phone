c417f6ccfab9345df936da0677412ec8394af54f
Semko, Georgii <ext-georgii.semko@here.com>
2021-04-22 01:05:16 +0300

    psi convertion

--- pressure/pressure.ino
+++ pressure/pressure.ino
@@ -17,17 +17,24 @@ void setup() {
 }
 
 void loop() {
-  int p1 = psiFromSensor(analogRead(pressure1Pin));
-  int p2 = psiFromSensor(analogRead(pressure2Pin));
-  int p3 = psiFromSensor(analogRead(pressure3Pin));
+  int a1 = analogRead(pressure1Pin);
+  int p1 = psiFromSensor(a1);
 
-  Serial.println("Pressure 1: " + (String) p1);
-  Serial.println("Pressure 2: " + (String) p2);
-  Serial.println("Pressure 3: " + (String) p3);
+  int a2 = analogRead(pressure2Pin);
+  int p2 = psiFromSensor(a2);
+
+  int a3 = analogRead(pressure3Pin);
+  int p3 = psiFromSensor(a3);
+
+  Serial.println("Pressure 1: " + (String) p1 + "psi; analog: " + (String) a1);
+  Serial.println("Pressure 2: " + (String) p2 + "psi; analog: " + (String) a2);
+  Serial.println("Pressure 3: " + (String) p3 + "psi; analog: " + (String) a3);
 
   digitalWrite(lockPin, p1 == pressure1Target && p2 == pressure2Target && p3 == pressure3Target ? LOW : HIGH);
+
+  delay(100);
 }
 
-void int psiFromSensor(int analogReading) {
+int psiFromSensor(int analogReading) {
   return analogReading / 13;
 }
